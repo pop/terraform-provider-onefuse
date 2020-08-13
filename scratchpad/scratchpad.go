786aac25a82dded6d05e5cde08383d970830c2af
Elijah Voigt <evoigt@cloudbolt.io>
2020-08-12 17:57:03 -0700

    Update logic kinda sorta work.
    Still need to figure out the best API we want to expose...
    
    Might involve making different datatypes for each type of request??

--- scratchpad/scratchpad.go
+++ scratchpad/scratchpad.go
@@ -11,6 +11,10 @@ func main() {
 	client := config.NewOneFuseApiClient()
 
 	// Add your stuff here
+    _, err := client.UpdateMicrosoftAdPolicy(6, &onefuse.MicrosoftAdPolicy{ComputerNameLetterCase: "UPPER", OU: "OU=NotOneFuse,DC=sovlabs,DC=net", Name: "NewName", Workspace: "/api/v3/onefuse/workspaces/1/"})
+    if err != nil {
+        fmt.Println(err)
+    }
 
 	fmt.Printf("Complete!")
 }
